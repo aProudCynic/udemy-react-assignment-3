bca2a3a155694794793ac7d891a440634355b7de
Balázs Márton <bmarton87@gmail.com>
2021-03-01 22:47:19 +0100

    feature: Add a simple navigation with two links => One leading to 'Users', one leading to 'Courses'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
@@ -8,10 +8,15 @@ class App extends Component {
   render () {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route path="/users"><Users/></Route>
-          <Route path="/courses"><Courses/></Route>
-        </Switch>
+        <div>
+          <nav>
+            <Link to={{pathname: '/users'}}>Users</Link> | <Link to={{pathname: '/courses'}}>Courses</Link>
+          </nav>
+          <Switch>
+            <Route path="/users"><Users/></Route>
+            <Route path="/courses"><Courses/></Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
